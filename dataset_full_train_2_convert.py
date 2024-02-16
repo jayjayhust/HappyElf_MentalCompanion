c9181e4318e8910bd6f1aa0fbf0723b9730b7d52
jayjayhust <jayhust@163.com>
2024-02-17 00:25:46 +0800

    update readme and synchronize dataset convert logic

--- dataset_full_train_2_convert.py
+++ dataset_full_train_2_convert.py
@@ -1,6 +1,7 @@
 # json数据加载：https://pythonjishu.com/ubrnbaxkgmxtyjb/
 
 import json
+import const
 
 file_train_2_path = './datasets/mental_health/PsyQA_full_train_2.json'  # 训练数据集1路径（10000条数据）
 file_train_2_converted_path = './datasets/mental_health/PsyQA_full_train_2_converted.json'  # 
@@ -37,12 +38,7 @@ def dataset_convert(file_path, file_converted_path):
             if "keywords"  in record:
                 del record["keywords"]
             
-            record['instruction'] = "现在你扮演一位专业的心理咨询师，你具备丰富的心理学和心理健康知识。" + \
-            "你擅长运用多种心理咨询技巧，例如认知行为疗法原则、动机访谈技巧和解决问题导向的短期疗法。" + \
-            "以温暖亲切的语气，展现出共情和对来访者感受的深刻理解。以自然的方式与来访者进行对话，" + \
-            "避免过长或过短的回应，确保回应流畅且类似人类的对话。提供深层次的指导和洞察，" + \
-            "使用具体的心理概念和例子帮助来访者更深入地探索思想和感受。避免教导式的回应，" + \
-            "更注重共情和尊重来访者的感受。根据来访者的反馈调整回应，确保回应贴合来访者的情境和需求。请为以下的对话生成一个回复。" + \
+            record['instruction'] = const.CHARACTER_DESCRIPTION + \
             "\n\n对话：\n来访者：{}。".format(record['question']) + \
             "\n咨询师："
             record['output'] = record['answers'][0]['answer_text']
